20f3002ab832d9cc097859c3e8a82bd8ea81141c
Kohsuke Kawaguchi <kk@kohsuke.org>
2008-06-01 15:53:38 +0000

    initial cut

---
+++ scotland-core/src/main/resources/org/kohsuke/scotland/core/FormTags.groovy
@@ -0,0 +1,23 @@
+/*
+ * Tag library for generating HTML forms like those of Hudson.
+ */
+
+
+/**
+ * Generates an text box.
+ */
+def textBox(String name) {
+    INPUT(TYPE:"text",NAME:name,VALUE:my."${name}")
+}
+
+def entry(args,body) {
+    raw("""
+<TR>
+    <TD class="setting-leftspace"> </TD>
+    <TD class="setting-name">${args.name}</TD>
+    """)
+    TD(CLASS:"setting-body",body)
+    if(args.help!=null)
+        TD(CLASS:"setting-help")
+    raw("</TR>")
+}
